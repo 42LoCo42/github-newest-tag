492c8c31a4d1cbf8ed5bdddc6483757a1bb7d14d
Leon Schumacher <leonsch@protonmail.com>
2024-10-23 15:57:04 +0200

    version 1

--- flake.nix
+++ flake.nix
@@ -2,11 +2,21 @@
   outputs = { flake-utils, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; }; in rec {
-        packages.default = pkgs.buildGoModule {
+        packages.default = pkgs.buildGoModule rec {
           pname = "github-newest-tag";
-          version = "0";
+          version = "1";
           src = ./.;
-          vendorHash = "";
+          vendorHash = "sha256-Phe6GXRi+mUk2bArydPR4II+NUhKI/+bOG12eqYBRz8=";
+
+          CGO_ENABLED = "0";
+
+          stripAllList = [ "bin" ];
+
+          meta = {
+            description = "Get the newest tag of a GitHub repo through the GraphQL API";
+            homepage = "https://github.com/42LoCo42/github-newest-tag";
+            mainProgram = pname;
+          };
         };
 
         devShells.default = pkgs.mkShell {
